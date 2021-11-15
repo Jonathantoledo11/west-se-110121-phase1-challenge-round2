3e863a224138a0d78a3ba10fac566f6069dc972f
Jonathan <jonathantoledo11@gmail.com>
2021-11-15 10:53:44 -0800

    submisson for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("you got this!");
+const url = "https://distinct-vaulted-freesia.glitch.me/image"
+const cardImg = document.getElementById("fg-image")
+const cardTitle = document.getElementById("fg-title")
+const commentList = document.getElementById("fg-comments")
+const likes = document.getElementById("fg-likes")
+const commentForm = document.getElementById("comment-form")
+const likeBtn = document.getElementById("like-button")
+
+fetch(url)
+.then(res => res.json())
+.then(renderPhoto)
+
+function renderPhoto(photo){
+    cardTitle.innerText = photo.title
+    cardImg.src = photo.image
+    commentList.innerHTML = 
+    `<li>${photo.comments[0].content}</li>
+    <li>${photo.comments[1].content}</li
+    <li>${photo.comments[2].content}</li>`
+    likeBtn.addEventListener("click", addLikes)
+
+
+ function addLikes(){
+    likes.innerText = ++photo.likes + "Likes"
+   
+    
+}
+
+commentForm.addEventListener("submit", postComment)
+function postComment(e){
+    e.preventDefault()
+    let newComment = document.createElement("li")
+    newComment.textContent = e.target.comment.value
+    commentList.append(newComment)
+    
+}
+}
